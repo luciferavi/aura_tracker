2fbc76d49be7aba3c284e0773e3fe259ceb140cb
raviastic <imravi3940@gmail.com>
2024-10-28 22:51:25 +0530

    CHECK

--- aura_tracker/src/App.js
+++ aura_tracker/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          AURA TRACKER        </p>
+          AURA         </p>
         <a
           className="App-link"
           href="https://reactjs.org"

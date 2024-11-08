f0ca02279ebac0da65f7722d78a0c35192bdfaa0
PIYUSHAG12345 <aggrawalshekhar432@gmail.com>
2024-11-08 17:46:27 +0530

    updatedsomepart

--- frontend/src/components/Achievements/Leaderboard.js
+++ frontend/src/components/Achievements/Leaderboard.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import './Leaderboard.css'
 const Leaderboard = () => {
   const [leaderboard, setLeaderboard] = useState([]);
   const [error, setError] = useState(null);
